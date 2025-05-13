5713e183c6c514779f5aa55589040dfeb7c39d72
DarjaElina <daria.elina98@gmail.com>
2025-05-13 14:04:24 +0300

    initial commit

---
+++ app/Services/CallApiService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class CallApiService {
+  public function getWeather($city) {
+    $apiKey = config('services.openweather.key');
+    $apiUrl = "http://api.openweathermap.org/data/2.5/weather?q={$city}&units=metric&appid={$apiKey}";
+
+    $response = Http::get($apiUrl);
+    return $response->successful()
+        ? $response->json()
+        : [];
+  }
+}
